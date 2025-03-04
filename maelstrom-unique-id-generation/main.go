cbb6970a2bf60d14e404bb06aa94ef2a54f550d2
Andrew Zhao <andrew2002zhao@gmail.com>
2025-03-04 16:20:40 -0500

    added unique id generation node

---
+++ maelstrom-unique-id-generation/main.go
@@ -0,0 +1,63 @@
+package main
+// a go program consists of packages where one of the packages are the main
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"context"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+func main() {
+	n := maelstrom.NewNode();
+
+	//create a handle to be added to the network
+	//this is like registering a handle callback
+	//use a lock
+	//and a number
+	//start at 0
+	
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		body := make(map[string]any)
+		var request map[string]any
+		if err := json.Unmarshal(msg.Body, &request); err != nil {
+				return err
+		}
+		//lock
+	
+		//call lin-tso service 
+		//get the ts back
+		unique_id_body := make(map[string]any)
+		unique_id_body["type"] = "ts"
+		ts_msg, ts_err := n.SyncRPC(context.Background(), "lin-tso", unique_id_body)
+		if ts_err != nil {
+			return ts_err
+		}
+		var ts map[string]any
+		if err := json.Unmarshal(ts_msg.Body, &ts); err != nil {
+			return err
+		}	
+		body["id"] = ts["ts"]
+		body["in_reply_to"] = request["msg_id"]
+		body["type"] = "generate_ok"
+
+
+		//increment
+	
+		//unlock
+		
+		//send out message
+		
+		
+
+		return n.Reply(msg, body)
+	})
+	
+	if err := n.Run(); err != nil {
+		log.Printf("ERROR %s", err);
+		os.Exit(1)
+	}
+
+	
+}
